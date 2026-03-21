f798f6c691ecdd63526670b278ff54a27af0a454
nsagnier <a2510954@institutmontilivi.cat>
2026-03-21 21:37:13 +0100

    EAC2: exercici4 completat

--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,17 +1,19 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule, } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-barra-cerca',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './barra-cerca.component.html',
   styleUrl: './barra-cerca.component.scss'
 })
 export class BarraCercaComponent {
   @Output() cercaCanviada = new EventEmitter<string>();
+  textCerca: string = '';
 
-  onCerca(event: Event) {
-    const input = event.target as HTMLInputElement;
-    this.cercaCanviada.emit(input.value);
+  onCerca() {
+    this.cercaCanviada.emit(this.textCerca);
   }
 }
