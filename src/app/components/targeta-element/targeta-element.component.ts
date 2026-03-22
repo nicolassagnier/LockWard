1a197c5d0e845ae72f7f0f66499379cf646af376
nsagnier <a2510954@institutmontilivi.cat>
2026-03-22 14:14:13 +0100

    EAC2: exercici 5 completat

--- src/app/components/targeta-element/targeta-element.component.ts
+++ src/app/components/targeta-element/targeta-element.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Element } from '../../models/element.model';
-import { NgIf, DatePipe, DecimalPipe, UpperCasePipe } from '@angular/common'
+import { NgIf, DatePipe, DecimalPipe, UpperCasePipe, NgClass } from '@angular/common'
 
 @Component({
   selector: 'app-targeta-element',
   standalone: true,
-  imports: [NgIf, DatePipe, DecimalPipe, UpperCasePipe],
+  imports: [NgIf, DatePipe, DecimalPipe, UpperCasePipe, NgClass],
   templateUrl: './targeta-element.component.html',
   styleUrl: './targeta-element.component.scss'
 })
